b6d259949655e0345111d002de3589ed3b3b0c19
Hamza Khan Lodhi <134397582+hamzalodhi2023@users.noreply.github.com>
2024-02-26 11:23:07 +0500

    page 1 completed

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-let page1Content = document.querySelector("#page1-content");
-let cursor = document.querySelector("#cursor");
+//? Mouse Effect Function Started
+function mouseEffect() {
+  let page1Content = document.querySelector("#page1-content");
+  let cursor = document.querySelector("#cursor");
 
-page1Content.addEventListener("mousemove", function (dats) {
-  gsap.to(cursor, {
-    x: dats.x,
-    y: dats.y,
+  page1Content.addEventListener("mousemove", function (dats) {
+    gsap.to(cursor, {
+      x: dats.x,
+      y: dats.y,
+    });
   });
-});
+
+  page1Content.addEventListener("mouseenter", function () {
+    gsap.to(cursor, {
+      scale: 1,
+      opacity: 1,
+    });
+  });
+  page1Content.addEventListener("mouseleave", function () {
+    gsap.to(cursor, {
+      scale: 0,
+      opacity: 0,
+    });
+  });
+}
+//` Mouse Effect Function ended
+mouseEffect();
